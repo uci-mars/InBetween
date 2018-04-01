806b5526c80e75ff96badc11283c2889e28b068a
Justin Lonh <justind6n423@gmail.com>
2018-04-01 00:00:24 -0700

    changed settings.py to add domain name and also added forms.py back into correct location

---
+++ InBetween/InBetweenApp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+class LocationAForm(forms.Form):
+    loc_a = forms.CharField(label='')
+    loc_a.widget.attrs.update({'align': 'center', 'placeholder':'Enter your address'})
+
+class LocationBForm(forms.Form):
+    loc_b = forms.CharField(label='')
+    loc_b.widget.attrs.update({'align': 'center', 'placeholder':'Enter other address'})
+
+class KeywordForm(forms.Form):
+    keyword = forms.CharField(label='', required=False)
+    keyword.widget.attrs.update({'align': 'center', 'placeholder':'Keyword'})
