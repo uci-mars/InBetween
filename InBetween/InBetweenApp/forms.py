6c582d74256107ea120e6dd8714a187ba66c67a7
Mars <32443134+uci-mars@users.noreply.github.com>
2018-07-02 23:38:05 -0700

    Update forms.py

--- InBetween/InBetweenApp/forms.py
+++ InBetween/InBetweenApp/forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 class LocationAForm(forms.Form):
     loc_a = forms.CharField(label='')
-    loc_a.widget.attrs.update({'align': 'center', 'placeholder':'Enter your address'})
+    loc_a.widget.attrs.update({'id': 'search_termA','align': 'center', 'placeholder':'Enter your address'})
 
 class LocationBForm(forms.Form):
     loc_b = forms.CharField(label='')
-    loc_b.widget.attrs.update({'align': 'center', 'placeholder':'Enter other address'})
+    loc_b.widget.attrs.update({'id': 'search_termB','align': 'center', 'placeholder':'Enter other address'})
 
 class KeywordForm(forms.Form):
     keyword = forms.CharField(label='', required=False)
-    keyword.widget.attrs.update({'align': 'center', 'placeholder':'Keyword'})
+    keyword.widget.attrs.update({'align': 'center', 'placeholder':'FIND tacos, cafe, donut, sushi, etc.'})
